54a79c46ff71fe96a23482416ba7caf25a6c8a77
Kakesn <mnelson55@mail.csuchico.edu>
2017-09-25 21:39:43 -0700

    guessingGame

--- guess.cpp
+++ guess.cpp
@@ -1,1 +1,33 @@
+//Author: Kekoa Nelson
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main () 
+{
+    int random;
+    int guess;
+    int guessminusrand;
+    int randminusguess;
+
+    srand (time (NULL));
+    random = rand() % 15 + 1;
+   
+    cout<< "Guess a number between 1 and 15:"<< guess<< endl;
+    cin>>  guess;
+
+    if (random == guess) { 
+    cout<< "You are correeeect!"; }  
+    else if (random > guess) {
+    randminusguess = random - guess;
+    cout<< "That s incorrect, you are off by: " << randminusguess<< endl; }  
+    else if (guess > random) {
+    guessminusrand = guess - random;
+    cout<< "That is incorrect, you are off by: " <<guessminusrand<< endl; }
+   
+
+    return 0;
+} 
+
 
